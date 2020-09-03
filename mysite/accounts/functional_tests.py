13db768cbaa7501101fe40f8145ff74b9b07cd93
Christian <CAlfano1999@gmail.com>
2020-09-03 15:36:56 -0400

    VCS version control using pycharm

---
+++ mysite/accounts/functional_tests.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    def tearDown(self):
+        self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+    # Edith has heard about a cool new online to-do app. She goes
+    # to check out its homepage
+        self.browser.get('http://localhost:8000')
+    # She notices the page title and header mention to-do lists
+        self.assertIn('COI Requests', self.browser.title)
+        self.fail('Finish the test!')
+    # She is invited to enter a to-do item straight away
+        #[...rest of comments as before]
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
